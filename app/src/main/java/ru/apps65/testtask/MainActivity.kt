c3ed0846408ce8f78e97abdd41f6debb9da83ef0
Aprekek <mhlshamburov@gmail.com>
2022-03-07 21:09:05 +0700

    added navigator

--- app/src/main/java/ru/apps65/testtask/MainActivity.kt
+++ app/src/main/java/ru/apps65/testtask/MainActivity.kt
@@ -2,18 +2,30 @@ package ru.apps65.testtask
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.androidx.AppNavigator
 import org.koin.android.ext.android.get
+import org.koin.android.ext.android.inject
 import ru.apps65.testtask.database.data.EmployeeKeyGenerator
 
 class MainActivity : AppCompatActivity() {
+
+	private val navigatorHolder: NavigatorHolder by inject()
+	private val navigator = AppNavigator(this, R.id.container)
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 	}
 
+	override fun onResume() {
+		super.onResume()
+		navigatorHolder.setNavigator(navigator)
+	}
+
 	override fun onPause() {
 		super.onPause()
+		navigatorHolder.removeNavigator()
 		get<EmployeeKeyGenerator>().saveKey()
 	}
 }
