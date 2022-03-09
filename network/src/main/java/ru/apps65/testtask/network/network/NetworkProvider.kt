efab3c65de077bf53693cb99eaaf3e2591b4ce9e
Aprekek <mhlshamburov@gmail.com>
2022-03-09 14:55:38 +0700

    made base url and moshi public; lazy retrofit initialization

--- network/src/main/java/ru/apps65/testtask/network/network/NetworkProvider.kt
+++ network/src/main/java/ru/apps65/testtask/network/network/NetworkProvider.kt
@@ -7,14 +7,16 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 object NetworkProvider {
 
-	private const val BASE_URL = "https://gitlab.65apps.com/65gb/static/raw/master/"
+	const val BASE_URL = "https://gitlab.65apps.com/65gb/static/raw/master/"
 
-	private val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-	val retrofit: Retrofit = Retrofit
-		.Builder()
-		.baseUrl(BASE_URL)
-		.addConverterFactory(MoshiConverterFactory.create(moshi))
-		.build()
+	val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+	val retrofit: Retrofit by lazy {
+		Retrofit
+			.Builder()
+			.baseUrl(BASE_URL)
+			.addConverterFactory(MoshiConverterFactory.create(moshi))
+			.build()
+	}
 
 	inline fun <reified T> createApi(): T = retrofit.create(T::class.java)
 }
